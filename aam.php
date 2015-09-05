a14cc33b274e64d75aabd056c7976a18c2669c13
uagarwal744 <uagarwal744@gmail.com>
2015-09-05 19:21:47 +0530

    AAM page added

---
+++ aam.php
@@ -0,0 +1,41 @@
+<head>
+    <link href="themes/5/js-image-slider.css" rel="stylesheet" type="text/css" />
+    <script src="themes/5/js-image-slider.js" type="text/javascript"></script>
+    <link href="generic.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<!--<?php
+	 include 'tophead.php';
+	 include 'navbar.php';
+	 ?>-->
+<br><br>
+<div id="sliderFrame">
+<br><br>
+        <div id="slider">
+            <a href="#">
+                <img src="img/2.jpg">
+            </a>
+            <a class="lazyImage" href="img/1.jpg" title="Customizable Transition Effects"></a>
+            <a href="#">
+                <b data-src="img/3.jpg" data-alt="#htmlcaption3"></b>
+            </a>
+            <a class="lazyImage" href="img/4.jpg" title="Pure Javascript. No jQuery. No flash."></a>
+            <a class="lazyImage" href="img/5.jpg" title="#htmlcaption5"></a>
+            <a class="lazyImage" href="img/6.jpg" title="Light weight Image Slider"></a>
+            <a class="lazyImage" href="img/7.jpg" title="Fine tuned. Sleek & Smooth"></a>
+            <a class="lazyImage" href="img/8.jpg" title="Easy-to-Use Slider"></a>
+        </div>
+		<div id="thumbs">
+            <div class="thumb"><img src="img/2.jpg"></div>
+            <div class="thumb"><img src="img/1.jpg"></div>
+            <div class="thumb"><img src="img/3.jpg"></div>
+            <div class="thumb"><img src="img/4.jpg"></div>
+            <div class="thumb"><img src="img/5.jpg"></div>
+            <div class="thumb"><img src="img/6.jpg"></div>
+            <div class="thumb"><img src="img/7.jpg"></div>
+            <div class="thumb"><img src="img/8.jpg"></div>
+        </div>
+</div>
+
+
+</body>
